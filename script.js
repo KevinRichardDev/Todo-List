de66aadaee2b42a8e80984e5d9d81258b5017cfe
KevinRichardDev <kevin.richard0@orange.fr>
2024-01-24 05:34:56 +0100

    Fin du projet TodoList

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const form = document.querySelector("form");
 
+// Stocker la liste dans le local storage (Storage part)
+function storeList() {
+  window.localStorage.todoList = list.innerHTML;
+}
+
+function getTodos () {
+  if (window.localStorage.todoList) {
+    list.innerHTML = window.localStorage.todoList;
+  } else {
+    list.innerHTML = `<li> Cliquez sur un todo pour le supprimer</li>`
+  }
+}
+
+// utiliser la function "getTodos" au chargement de la page.
+window.addEventListener('load', getTodos);
+
 // Add Element
 form.addEventListener("submit", (e) => {
   // Enlever la réactualisation de la page lors de la validation de l'input
@@ -8,8 +24,8 @@ form.addEventListener("submit", (e) => {
   list.innerHTML += `<li>${item.value}</li>`;
   // vider l'input aprés validation
   item.value = "";
+  storeList();
 })
-
 
 // Supprimer un element au click
 
@@ -22,5 +38,7 @@ list.addEventListener("click", (e) => {
     // Ajoute une classe ou l'on click
     e.target.classList.add("checked");
   }
-  
+  storeList();
 })
+
+
