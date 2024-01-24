0b26a70c29dd48345afa8a90a2eb681d32478bb6
KevinRichardDev <kevin.richard0@orange.fr>
2024-01-24 05:20:00 +0100

    list(click)

--- script.js
+++ script.js
@@ -4,6 +4,23 @@ const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
   // Enlever la réactualisation de la page lors de la validation de l'input
   e.preventDefault();
+  // injecter la valeur de l'input dans l'ul.
+  list.innerHTML += `<li>${item.value}</li>`;
+  // vider l'input aprés validation
+  item.value = "";
+})
 
-  list.innerHTML = `<li>${item.value}</li>`
+
+// Supprimer un element au click
+
+list.addEventListener("click", (e) => {
+
+  if(e.target.classList.contains("checked")){
+    // Supprime le li que l'on vient de créer
+    e.target.remove();
+  } else {
+    // Ajoute une classe ou l'on click
+    e.target.classList.add("checked");
+  }
+  
 })
